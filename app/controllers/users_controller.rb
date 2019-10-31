3fb3d0c6699d2429850ce01732445230936054f8
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-10-31 14:07:09 -0300

    rubocop fixed, no offenses detected

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  def index
+    @name = 'You are on the index page!'
+  end
 
-    def index
-        @name = "You are on the index page!"
-    end
+  def show
+    @name = 'You are on the show page!'
+  end
 
-    def show
-        @name = "You are on the show page!"
-    end
+  def new
+    @name = 'You are on the new page!'
+  end
 
-    def new
-        @name = "You are on the new page!"
-    end
+  def edit
+    @name = 'You are on the edit page!'
+  end
 
-    def edit
-        @name = "You are on the edit page!"
-    end
-
-    def create
-    end
+  def create; end
 end
